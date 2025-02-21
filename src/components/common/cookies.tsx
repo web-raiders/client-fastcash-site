a07076e179d4d769eda22bbc7dd9781334a51f02
dchima <dcnnad@gmail.com>
2025-02-21 09:58:58 +0100

    stable

--- src/components/common/cookies.tsx
+++ src/components/common/cookies.tsx
@@ -22,10 +22,9 @@ const Cookies = () => {
                 <h2 className="text-[26px] font-bold mb-3">We use cookies 🍪</h2>
                 <div>
                   We use cookies to ensure you have the best browsing experience on our website.
-                  By using our site, you acknowledge that you have read and understood our <a href='/policy' className="text-[#0fcc7c] font-bold">Cookie Policy</a> & <a href='/policy' className="text-[#0fcc7c] font-bold">Privacy Policy</a>
                 </div>
                 <div className='flex gap-5 mt-7'>
-                  <div className="py-2 px-5 font-semibold rounded-[10px] cursor-pointer border border-[#0fcc7c] text-[#0fcc7c] hover:text-white hover:bg-[#0fcc7c]" onClick={() => handleCookies('accept')}>Accept</div>
+                  <div className="py-2 px-5 font-semibold rounded-[10px] cursor-pointer border border-[#F56B1E] text-[#F56B1E] hover:text-white hover:bg-[#F56B1E]" onClick={() => handleCookies('accept')}>Accept</div>
                   <div className="py-2 px-5 font-semibold rounded-[10px] cursor-pointer border border-red-400 text-red-400 hover:text-white hover:bg-red-400" onClick={() => handleCookies('decline')}>Decline</div>
                 </div>
               </div>
