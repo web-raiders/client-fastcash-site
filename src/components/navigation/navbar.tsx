900a1cbc5c09c0e0419aafe9caf7e2b93e24ec50
dchima <dcnnad@gmail.com>
2025-03-11 08:36:39 +0100

    fixes

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -66,7 +66,7 @@ const Navbar = () => {
             <div className="_container">
                 <div className="_flex_box">
                     <div className='_logo'>
-                        <a href='/'><img src={Logo} alt='logo' width={"120"} height={"120"} /></a>
+                        <a href='/'><img src={Logo} alt='logo' width={"140"} height={"140"} /></a>
                     </div>
                     <div className="_nav_menu">
                         <ul className="_nav_list">
