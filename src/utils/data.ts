9be99c7676a228298428e846d8e95f4ffa45ce79
dchima <dcnnad@gmail.com>
2025-03-14 07:02:29 +0100

    fix

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -113,7 +113,7 @@ export const contactList = [
     {
         icon: SlLocationPin,
         title: 'Head Office',
-        headingOne: 'Ikeja Town Square Mall, 131 Obafemi Awolowo Way Ikeja',
+        headingOne: 'Ikeja Town Square, 131 Obafemi Awolowo Way Ikeja',
         headingTwo: 'Lagos Nigeria',
     },
     {
